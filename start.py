3d85d3da951e3c2c6815d136b0f1664bd1b84265
rewolfer <johndoe@example.com>
2020-04-10 15:44:17 +0300

    13

--- start.py
+++ start.py
@@ -1,1 +1,9 @@
-print("hello world")
+import math
+a = 4.1
+b = 2.7
+x = 1.2
+
+def calc(a, b, x):
+    y = (a*math.sqrt(x)-b*math.log(5, x))/(math.log(math.fabs(x-1)))
+    print(x,y)
+calc(a, b, x)
