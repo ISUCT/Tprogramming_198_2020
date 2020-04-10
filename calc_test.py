ccfd0a6ca33c3a616f73048bb098a9862b458b5e
rewolfer <johndoe@example.com>
2020-04-10 18:31:03 +0300

    13

---
+++ calc_test.py
@@ -0,0 +1,28 @@
+import unittest
+import start
+
+class CalcTests(unittest.TestCase):
+    a = 2.7
+    b = 4.1
+    def test_possitive(self):
+        res = start.calc(2,2,1)
+        self.assertAlmostEqual(1.099,res, 3)
+    def test_normal_b(self):
+        x_lst=[1.9, 2.15, 2.34, 2.73, 3.16]
+        res = task_b(self.a,self.b,x_lst)
+        self.assertEqual(5, len(res))
+
+    def test_zero_length(self):
+            x_lst = []
+            res = task_b(self.a,self.b,x_lst)
+            self.assertEqual(0, len(res))
+
+    def test_normal_a(self):
+            res = self.task.a(self.a, self.b, 0.77, 1.77, 0.2)
+            self.assertEqual(6, len(res))
+
+    def test_zero_a(self):
+            res = self.task.a(self.a, self.b, 4.77, 1.77, 0.2)
+            self.assertEqual(0, len(res))
+if __name__ == '__main__':
+    unittest.main()
