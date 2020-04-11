dd0dcaaa104538fa5a33b1153eb9d9015fc936ce
Natalia <johndoe@example.com>
2020-04-11 09:57:48 +0300

    pigg

---
+++ pig.py
@@ -0,0 +1,45 @@
+class Pig:
+    
+    def __init__(self, name):
+        self.name = name
+        self.__weight = 0
+        print("Создание объекта")
+    
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        if isinstance(weight,(int, float))and weight >= 0 and weight <= 10:
+            self.__weight = weight
+        else:
+            print("Не правильно указано колличество")
+    
+    def increase_weight(self):
+        self.__weight = self.__weight + 1
+
+    def __str__(self):
+        return f"name: {self.name}, weight: {self.__weight}" + """
+           _
+         <`--'\>______
+         /. .  `'     \.
+        (`')  ,        @
+         `-._,        /
+            )-)_/--( >  jv
+           ''''  ''''"""
+    
+
+
+if __name__ == "__main__":
+    pig1 = Pig("Rita")
+    pig1.weight = 10
+    pig1.increase_weight()
+    print(pig1.name, pig1.weight)
+
+    pig2 = Pig("Rata")
+    pig2.weight = 2.3
+    pig2.increase_weight()
+    print(pig2.name, pig2.weight)
+    
+    print(pig2)
