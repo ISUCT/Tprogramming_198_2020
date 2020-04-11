ee39660da80f5359ec2f08a2b65dad731c1df180
John Doe <johndoe@example.com>
2020-04-11 10:27:08 +0300

    123

--- fish.py
+++ fish.py
@@ -21,26 +21,32 @@ class Country:
 
     def __str__(self):
         return f"city: {self.city}, population: {self.__population}" + """
-      /`·.¸
-     /¸...¸`:·
- ¸.·´  ¸   `·.¸.·´)
-: © ):´;      ¸  {
- `·.¸ `·  ¸.·´\`·¸)
-     `\´´\¸.·´
-        """
+     ^
+                _______ ^^^
+               | Ххххххй | _ ^^^^^ _
+               | Ххххххй | | [] [] |
+            ______ ххххх | | [] [] [] |
+           | ++++++ | хххх | | [] [] [] | МЕТРОПОЛИС
+           | ++++++ | хххх | | [] [] [] |
+           | ++++++ | _________ [] [] |
+           | ++++++ | = | = | = | = | = | [] |
+           | ++++++ | = | = | = | = | = | [] [] |
+___________ | ++ HH ++ | _HHHH__ | _________ _________ _________
+         _______________ ______________ ______________
+__________________ ___________ __________________ ____________
 
 if __name__ == "__main__":
-    city1 = City("Toronto")
+    city1 = Country("Toronto")
     # city1.population = 200000
     city1.increase_population()
-    print(City1.name, city1.population)
+    print(City1.city, city1.population)
 
-    city2 = City("Moskow")
+    city2 = Country("Moskow")
     # city2.population = 300000
     city2.increase_population()
-    print(city2.name, city2.population)
+    print(city2.city, city2.population)
 
-    city2.population = 30000
+    city2.population = 300000
     city2.population = "Test"
 
-    print(city2)
+    print("city2")
