5210caac3eb5ba4eb65723db724f2ac6152591e1
Natalia <johndoe@example.com>
2020-04-11 10:37:03 +0300

    piggg

--- pig.py
+++ pig.py
@@ -19,6 +19,9 @@ class Pig:
     def increase_weight(self):
         self.__weight = self.__weight + 1
 
+    def run(self):
+        print(f"я {self.name}, я бегу, чтобы мня не убили")
+
     def __str__(self):
         return f"name: {self.name}, weight: {self.__weight}" + """
            _
@@ -36,10 +39,12 @@ if __name__ == "__main__":
     pig1.weight = 10
     pig1.increase_weight()
     print(pig1.name, pig1.weight)
+    pig1.run()
 
     pig2 = Pig("Rata")
     pig2.weight = 2.3
     pig2.increase_weight()
     print(pig2.name, pig2.weight)
+    pig2.run()
     
     print(pig2)
