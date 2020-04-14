f0a6f219dde4e75cbe1a5c16bac6269103c8cbac
Natalia <johndoe@example.com>
2020-04-14 15:16:53 +0300

    html

--- app.py
+++ app.py
@@ -2,6 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 import datetime
 from flask import render_template
+import pig as Pig
+
+collection = []
+def init():
+    pig1 = Pig.Pig("Rita")
+    collection.append(pig1)
+    pig2 = Pig.Pig("Rata")
+    collection.append(pig2)
+    pig3 = Pig.Pig("bigPig")
+    collection.append(pig3)
+
+init()
+
 
 @app.route('/')
 def hello_world():
@@ -35,3 +48,30 @@ def calc(type_calc):
         res = a*b
         sign = "X"
     return render_template('calc.html', result=res, sign=sign)
+
+@app.route("/pigs")
+def pigs():
+    return render_template("pigs.html", collection=collection)
+
+from flask import redirect
+@app.route("/pigs/new", methods=["GET", "POST"])
+def pig_add():
+    print("Test")
+    name = request.form.get("name", default="", type=str)
+    age = request.form.get("age", default=0, type=float)
+    if name:
+        print("Got Name")
+        pig = Pig.Pig(name)
+        pig.age = age
+        collection.append(pig)
+        return redirect("/pigs")
+    return render_template("new.html")
+
+@app.route("/pigs/<name>")
+def pig(name):
+    item = None
+    for el in collection:
+        if name == el.name:
+            item = el
+            break
+    return render_template("pig.html", item=item)
