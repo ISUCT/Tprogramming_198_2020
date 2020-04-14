aaed94486ef2c2e79ea01ac5c767b7efae90f334
rewolfer <johndoe@example.com>
2020-04-14 11:35:32 +0300

    и снова коммит

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 import datetime
 from flask import render_template
+import Employee
 
+collection = []
+def init():
+    employee = Employee.Employee("Boris")
+    collection.append(employee)
+    employee = Employee.Employee("Olga")
+    collection.append(employee)
+    employee = Employee.Employee("Ivi")
+    collection.append(employee)
+
+init()
 
 @app.route('/')
 def hello_world():
@@ -48,3 +59,18 @@ def calc(type_calc):
         sign = "/"
 
     return render_template('calc.html', result=res, sign=sign)
+@app.route("/employees")
+def employees():
+    print (collection)
+    return render_template("employees.html", collection=collection)
+
+@app.route("/employees/new", methods=["GET", "POST"])
+def employee_add():
+    name = request.form.get("name", default="", type=str)
+    date = request.form.get("date", default=0, type=float)
+    if name:
+        employee = Employee.Employee(name)
+        employee.date = date
+        collection.append(employee)
+
+    return render_template("new.html")
