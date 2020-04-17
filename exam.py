7833f6eee0e5ed68de5a0ca3c98311e51a780b4f
Natalia <johndoe@example.com>
2020-04-17 17:05:35 +0300

    emax.py

---
+++ exam.py
@@ -0,0 +1,7 @@
+ninjas = 5 
+if ninjas <=10:
+    print("я одолею этих ниндзя")
+elif ninjas <=30:
+    print("будет не просто, но я с ними разделаюсь")
+elif ninjas <=50:
+    print("их слишком много")
