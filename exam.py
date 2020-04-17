4f32909996967cae9f602ebd1e0dee8616efcf5a
Lidia.zel <lidia.zel@mail.ru>
2020-04-17 17:42:07 +0400

    exam1

--- exam.py
+++ exam.py
@@ -1,5 +1,5 @@
 print("Рецепт бутерброда:")
-ingredients = ['хлеб', 'масло', 'сыр']
+ingredients = ['слизни', 'пиявки', 'катышки из пупка гориллы', 'брови гусеницы', 'пальцы многоножки']
 length = len(ingredients)
 for i in range(length):
     print(str(i+1), ") ", ingredients[i], sep="", end=";\n")
