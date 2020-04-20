9dcf4f8c28adf9b0f7f41f9f6fc9c1ad08f4d651
KoptsevaAlena <a.kopceva99@mail.ru>
2020-04-20 10:24:06 +0300

    test_9.py

---
+++ test_9.py
@@ -0,0 +1,10 @@
+class counter:
+    def counter(self, n=0):
+        for i in range(0, n+1):
+            print(i)
+        while i >-1:
+            print(i)
+            i -= 2  
+my_counter = counter()
+my_counter.counter(10)
+    
