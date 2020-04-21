72bfe76975112bbcae2371459799d3463a0bac08
svetlanakiri <63354639+svetlanakiri@users.noreply.github.com>
2020-04-21 10:53:13 +0300

    Add files via upload

---
+++ exam.py
@@ -0,0 +1,27 @@
+print("metod")
+import math
+a = 3
+b = 2
+x = 1
+
+while x <= 10:
+    print(x)
+    x = x + 1 
+    
+n = 10
+for x in range (1,n + 1):
+    print (f"{x} - {b} = {x - b}")
+    
+n = 10
+for x in range (1,n + 1):
+    print (f"{a} * {x - b} = {a * (x - b)}")
+
+n = 10
+for x in range (1,n + 1):
+    print (f"1 + {a * (x - b)} = {1 + (a * (x - b))}")
+
+n = 10
+for x in range (1,n + 1):
+    print (f"1 + {a * (x - b)} = {1 + (a * (x - b))}")
+
+print_result(res)
